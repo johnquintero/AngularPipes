c8252468eb1eb1842247ccfba486380209ae8292
johnkintero <johnjairoqp@gmail.com>
2020-07-18 18:24:08 -0500

    ejemplos pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre      : string ='Capitan America';
+  autor       : string = 'johN jaIro quiNTero';
+  arreglo     : number[] = [1,2,3,4,5,6,7,8,9,0];
+  PI          : number = Math.PI;
+  porcentaje  : number = 0.234;
+  salario     : number = 1234.5;
+  idioma      : string = 'es';
+  valorPromesa = new Promise<string>((resolve)=>{
+      setTimeout(() => {
+        resolve('LLego la data');
+      }, 4500);
+  });
+
+  fecha : Date = new Date();
+
+  heroe = {
+    nombre : 'Logan',
+    clave  : 'wolverine',
+    edad   : 45,
+    equipos : {
+      inicial :'X-men',
+      segundo : 'x-force'
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from "@angular/common";
+
+//idiomas que podra manejar
+import  localeCo  from "@angular/common/locales/es";
+import  localeFr  from "@angular/common/locales/fr";
+
+registerLocaleData(localeCo);
+registerLocaleData(localeFr);
+
 
 import { AppComponent } from './app.component';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue:'es-CO'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
